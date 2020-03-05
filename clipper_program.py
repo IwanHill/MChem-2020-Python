d17e5a71c3f13080e04a19db5c01b38058d83897
IwanHill <iah510@york.ac.uk>
2020-03-05 12:02:42 +0000

    Added sidebar for residue numbering. Not sure this is the best execution, can fix later.

--- clipper_program.py
+++ clipper_program.py
@@ -8,29 +8,18 @@ def clipper_readin(filename): #from Hackers tutorial
     model_file.import_minimol(molecule)
     return molecule
 
-def fasta(molecule):
-    model = molecule.model()
-    sequence = "" #need something to add residues to
-    #counter = 0
+def fasta(polymer):
+    sequence = "" #need something to add residues to.
     residues = dict(ALA = "A", ARG = "R", HIS = "H", LYS = "K", ASP = "D",
     GLU = "E", SER = "S", THR = "T", ASN = "N", GLN = "Q", PRO = "P",
     VAL = "V", ILE = "I", LEU = "L", MET = "M", PHE = "F", TYR = "Y",
     TRP = "W", CYS = "C", GLY = "G", UNK = "X") #dictionary of natural AAs.
-    for polymer in model:
-        #sequence += "<br>"
-        #counter += 1
-        #sequence += "chain " + str(counter) + ": <br>"
-        for monomer in polymer:
-            if monomer.type().trim() in residues:
-                sequence += residues[monomer.type().trim()]
-            else:
-                pass #cuts out *all* non standard amino acids, heteroatoms, worth looking at later,
+    for monomer in polymer:
+        if monomer.type().trim() in residues:
+            sequence += residues[monomer.type().trim()]
+        else:
+            pass #cuts out *all* non standard amino acids, heteroatoms, worth looking at later,
     return sequence
-
-def line_breaker(input): #slices and rejoins every 80 characters with 2 HTML line breaks
-    string_record = str(input)
-    string_record = '</div><br><div style="font-family:courier; font-size:2vw;">'.join(string_record[i:i + 80] for i in range(0, len(string_record), 80))
-    return string_record
 
 def get_colour(residue):
     if residue in ("D", "E"):
@@ -57,7 +46,6 @@ def write_boilerplate_start(filename = "2DREP_Output.html"):
                 </head>
                 <body>
                 <img src="2DREP_logo.png" alt="2DREP Logo" style="width:40%;height:25%;">
-                    <div style="font-family:courier; font-size:2vw;">
                 """
                 ) #Working title is 2DREP
                 #logo styling is not working inline but other bits are...
@@ -70,28 +58,42 @@ def write_boilerplate_end(filename = "2DREP_Output.html"):
         </body>
     </html>""") #close HTML
 
-def write_data(filename = "2DREP_Output.html", sequence = None):
-    counter = 0 #allows us to integrate line_breaker functionality into our
+def write_data(filename = "2DREP_Output.html", molecule = None):
+    model = molecule.model()
     with open(filename, "a") as output:
-        for residue in sequence:
-            if counter == 0:
-                output.write("""
-                S A M P L E L E T T E R S ~ALPHA1~ <img src="sample_helix.png" alt="sample_helix" style="width:10%;heigth:10%">
-                _BETA1_ <img src="sample_sheet.png" alt="sample_sheet" style="width:10%;heigth:10%"> </div><div style="font-family:courier;
-                font-size:2vw;"> """)
-            #call some function that will write the  annotations we want! Probs need to check in with Jon for this...
-            counter += 1
-            colour = get_colour(residue)
-            if colour:
+        for polymer in model:
+            character_number = 78
+            counter = 0 #allows us to integrate line_breaker functionality into our writer
+            residue_count = 0
+            output.write("""<div style="font-family:courier; font-size:2vw; text-decoration:underline;"><br> Chain """)
+            output.write(str(polymer.id().trim()))
+            output.write(": </div>")
+            sequence = fasta(polymer)
+            for residue in sequence:
+                residue_count += 1
+                if counter == 0:
+                    output.write("""
+                    <div style="font-family:courier; font-size:2vw;">
+                    &nbsp;&nbsp;&nbsp;&nbsp;S A M P L E L E T T E R S ~ALPHA1~ <img src="sample_helix.png" alt="sample_helix" style="width:10%;heigth:10%">
+                    _BETA1_ <img src="sample_sheet.png" alt="sample_sheet" style="width:10%;heigth:10%"> </div><div style="font-family:courier;
+                    font-size:2vw;"> """)
+                    #call some function that will write the  annotations we want! Probs need to check in with Jon for this...
+                    output.write(str(residue_count))
+                    if residue_count < 100:
+                        output.write("&nbsp;")
+                    if residue_count < 10:
+                        output.write("&nbsp;")
+                    output.write("&nbsp")
+                colour = get_colour(residue)
                 output.write("<span style=\"color:" + colour + "\">" + residue + "</span>") #colours with the appropriate hexcode for our residue
-            else:
-                output.write(residue)
-            if counter == 80:
-                output.write("</div><div style=\"font-family:courier; font-size:2vw;\">")
-                counter = 0
+                counter += 1
+                if len(str(residue_count)) != 1 and len(str(residue_count)) != 2:
+                    character_number = 79
+                if counter == (character_number - len(str(residue_count))):
+                    output.write("</div>")
+                    counter = 0
 
 molecule = clipper_readin(sys.argv[1]) #reads in file from command line
-sequence = fasta(molecule)
 write_boilerplate_start(sys.argv[2])
-write_data(sys.argv[2], sequence)
+write_data(sys.argv[2], molecule)
 write_boilerplate_end(sys.argv[2])
