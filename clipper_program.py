398c2512d6173e171509394d238a04b7a76b86a7
IwanHill <iah510@york.ac.uk>
2020-03-17 23:35:08 +0000

    > Refactored get_colour > added B factor based background colours! > added legend > edited logo a little > adjusted residue counter sidebar, still not happy w/ it

--- clipper_program.py
+++ clipper_program.py
@@ -1,4 +1,5 @@
 import sys
+import math
 from clipper_python import _clipper as clipper
 
 def clipper_readin(filename): #from Hackers tutorial
@@ -22,18 +23,52 @@ def fasta(polymer):
     return sequence
 
 def get_colour(residue):
-    if residue in ("D", "E"):
-        return("#D71313") #negative chains in red
-    elif residue in ("R", "H", "K"):
-        return("#0027FF") #positive side chains in dark blue
-    elif residue in ("S", "T", "N", "Q"):
-        return("#15E4EA") #polar side chains in sky blue
-    elif residue in ("C", "U", "G", "P"):
-        return("#FF00AB") #special side chains in hot pink
-    elif residue in ("A", "V", "I", "L", "M", "F", "Y", "W"):
-        return("#C6BE2A") #hydrophobic side chains in oily yellow
+    colours = dict(D = "#D71313", E = "#D71313", R = "#0027FF",
+    H = "#0027FF", K = "#0027FF", S = "#15E4EA", T = "#15E4EA",
+    N = "#15E4EA", Q = "#15E4EA", C = "#FF00AB", U = "#FF00AB",
+    G = "#FF00AB", P = "#FF00AB", A = "#C6BE2A", V = "#C6BE2A",
+    I = "#C6BE2A", L = "#C6BE2A", M = "#C6BE2A", F = "#C6BE2A",
+    Y = "#C6BE2A", W = "#C6BE2A") # in order:
+    #negative chains in red #positive side chains in dark blue
+    #polar side chains in sky blue #special side chains in hot pink
+    #hydrophobic side chains in oily yellow #won't style unknown or unnatrual AAs.
+    if residue in colours:
+        return colours[residue]
     else:
-        return False #won't throw style at any unnatural amino acid or formatting characters.
+        return "#000000"
+
+def calc_avg_B(monomer):
+    atom_Bs = []
+    for atom in monomer:
+        u = atom.u_iso #draws u value out - don't use brackets!
+        bfac = u * 8 * (math.pi)**2 #convert U factor to B factor for atom.
+        atom_Bs.append(bfac)
+    avg_B = round(sum(atom_Bs)/len(atom_Bs), 1) #brute force calulation of mean B factor
+    return avg_B #need to call function within loops.
+
+def background_B_colour(avg_B):
+    if avg_B <= 5:
+        return("#FFFFFF")
+    if avg_B >= 5 and avg_B <= 15:
+        return("#E6E6E6")
+    if avg_B >= 15 and avg_B <= 25:
+        return("#CCCCCC")
+    if avg_B >= 25 and avg_B <= 35:
+        return("#B3B3B3")
+    if avg_B >= 35 and avg_B <= 45:
+        return("#999999")
+    if avg_B >= 45 and avg_B <= 55:
+        return("#808080")
+    if avg_B >= 55 and avg_B <= 65:
+        return("#666666")
+    if avg_B >= 65 and avg_B <= 75:
+        return("#4D4D4D")
+    if avg_B >= 75 and avg_B <= 85:
+        return("#333333")
+    if avg_B >= 85 and avg_B <= 95:
+        return("#1A1A1A")
+    if avg_B >= 95:
+        return("#000000")
 
 def write_boilerplate_start(filename = "2DREP_Output.html"):
     with open(filename, "a") as output: #use "a" to append, prevents overwriting.
@@ -45,7 +80,7 @@ def write_boilerplate_start(filename = "2DREP_Output.html"):
                 <title>"2DREP"</title>
                 </head>
                 <body>
-                <img src="2DREP_logo.png" alt="2DREP Logo" style="width:40%;height:25%;">
+                <img src="logo_and_legend.png" alt="2DREP Logo" style="width:100%;height:40%;">
                 """
                 ) #Working title is 2DREP
                 #logo styling is not working inline but other bits are...
@@ -62,13 +97,16 @@ def write_data(filename = "2DREP_Output.html", molecule = None):
     model = molecule.model()
     with open(filename, "a") as output:
         for polymer in model:
-            character_number = 78
+            character_number = 76
             counter = 0 #allows us to integrate line_breaker functionality into our writer
-            residue_count = 0
+            residue_count = 0 #tracked separately, won't reset with each new line.
             output.write("""<div style="font-family:courier; font-size:2vw; text-decoration:underline;"><br> Chain """)
             output.write(str(polymer.id().trim()))
             output.write(": </div>")
-            sequence = fasta(polymer)
+            bfacs_list = [] #we'll pull a B factor for display later
+            for monomer in polymer: #need to act before our residues are a string
+                bfacs_list.append(calc_avg_B(monomer)) #adds the b factor for each residue in the ATOM list - will match our sequence!
+            sequence = fasta(polymer) #we'll write out this string styled
             for residue in sequence:
                 residue_count += 1
                 if counter == 0:
@@ -85,10 +123,11 @@ def write_data(filename = "2DREP_Output.html", molecule = None):
                         output.write("&nbsp;")
                     output.write("&nbsp")
                 colour = get_colour(residue)
-                output.write("<span style=\"color:" + colour + "\">" + residue + "</span>") #colours with the appropriate hexcode for our residue
+                background = background_B_colour(bfacs_list[residue_count - 1]) #residue count starts at 1, indices start at 0.
+                output.write("<span style=\"color:" + colour + "; background-color:" + background + "\">" + residue + "</span>") #colours with the appropriate hexcode for our residue
                 counter += 1
                 if len(str(residue_count)) != 1 and len(str(residue_count)) != 2:
-                    character_number = 79
+                    character_number = 77 #ends up with 74 residues to a line, so new lines are on multiples of 75, for easier counting.
                 if counter == (character_number - len(str(residue_count))):
                     output.write("</div>")
                     counter = 0
