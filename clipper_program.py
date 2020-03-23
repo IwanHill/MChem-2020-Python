ad888c9802046c27a287c201ba83916ebb09d71c
IwanHill <iah510@york.ac.uk>
2020-03-23 15:44:36 +0000

    Now scales B factors relative to the protein in qeustion - but it's a hack job!

--- clipper_program.py
+++ clipper_program.py
@@ -2,6 +2,13 @@ import sys
 import math
 from clipper_python import _clipper as clipper
 
+#set a dictionary of residues as a global variable
+#allows multiple functions to draw from same dictionary
+residues = dict(ALA = "A", ARG = "R", HIS = "H", LYS = "K", ASP = "D",
+GLU = "E", SER = "S", THR = "T", ASN = "N", GLN = "Q", PRO = "P",
+VAL = "V", ILE = "I", LEU = "L", MET = "M", PHE = "F", TYR = "Y",
+TRP = "W", CYS = "C", GLY = "G", UNK = "X") #dictionary of natural AAs.
+
 def clipper_readin(filename): #from Hackers tutorial
     model_file = clipper.MMDBfile()
     model_file.read_file(filename) #supports .pdb and .cif by default
@@ -10,16 +17,12 @@ def clipper_readin(filename): #from Hackers tutorial
     return molecule
 
 def fasta(polymer):
-    sequence = "" #need something to add residues to.
-    residues = dict(ALA = "A", ARG = "R", HIS = "H", LYS = "K", ASP = "D",
-    GLU = "E", SER = "S", THR = "T", ASN = "N", GLN = "Q", PRO = "P",
-    VAL = "V", ILE = "I", LEU = "L", MET = "M", PHE = "F", TYR = "Y",
-    TRP = "W", CYS = "C", GLY = "G", UNK = "X") #dictionary of natural AAs.
+    sequence = "" #need something to add residues to
     for monomer in polymer:
         if monomer.type().trim() in residues:
             sequence += residues[monomer.type().trim()]
         else:
-            pass #cuts out *all* non standard amino acids, heteroatoms, worth looking at later,
+            pass #cuts out *all* non standard amino acids, heteroatoms, worth looking at later
     return sequence
 
 def get_colour(residue):
@@ -31,7 +34,7 @@ def get_colour(residue):
     Y = "#C6BE2A", W = "#C6BE2A") # in order:
     #negative chains in red #positive side chains in dark blue
     #polar side chains in sky blue #special side chains in hot pink
-    #hydrophobic side chains in oily yellow #won't style unknown or unnatrual AAs.
+    #hydrophobic side chains in oily yellow #won't style unknown or unnatrual AAs
     if residue in colours:
         return colours[residue]
     else:
@@ -46,29 +49,32 @@ def calc_avg_B(monomer):
     avg_B = round(sum(atom_Bs)/len(atom_Bs), 1) #brute force calulation of mean B factor
     return avg_B #need to call function within loops.
 
-def background_B_colour(avg_B):
-    if avg_B <= 5:
-        return("#FFFFFF")
-    if avg_B >= 5 and avg_B <= 15:
-        return("#E6E6E6")
-    if avg_B >= 15 and avg_B <= 25:
-        return("#CCCCCC")
-    if avg_B >= 25 and avg_B <= 35:
-        return("#B3B3B3")
-    if avg_B >= 35 and avg_B <= 45:
-        return("#999999")
-    if avg_B >= 45 and avg_B <= 55:
-        return("#808080")
-    if avg_B >= 55 and avg_B <= 65:
-        return("#666666")
-    if avg_B >= 65 and avg_B <= 75:
-        return("#4D4D4D")
-    if avg_B >= 75 and avg_B <= 85:
-        return("#333333")
-    if avg_B >= 85 and avg_B <= 95:
-        return("#1A1A1A")
-    if avg_B >= 95:
-        return("#000000")
+def background_B_colour(list_of_Bs):
+    bkgrnd_colours = []
+    for avg_B in list_of_Bs:
+        if avg_B <= (0.05 * max(list_of_Bs)):
+            bkgrnd_colours.append("#FFFFFF")
+        if avg_B >= (0.05 * max(list_of_Bs)) and avg_B <= (0.15 * max(list_of_Bs)):
+            bkgrnd_colours.append("#E6E6E6")
+        if avg_B >= (0.15 * max(list_of_Bs)) and avg_B <= (0.25 * max(list_of_Bs)):
+            bkgrnd_colours.append("#CCCCCC")
+        if avg_B >= (0.25 * max(list_of_Bs)) and avg_B <= (0.35 * max(list_of_Bs)):
+            bkgrnd_colours.append("#B3B3B3")
+        if avg_B >= (0.35 * max(list_of_Bs)) and avg_B <= (0.45 * max(list_of_Bs)):
+            bkgrnd_colours.append("#999999")
+        if avg_B >= (0.45 * max(list_of_Bs)) and avg_B <= (0.55 * max(list_of_Bs)):
+            bkgrnd_colours.append("#808080")
+        if avg_B >= (0.55 * max(list_of_Bs)) and avg_B <= (0.65 * max(list_of_Bs)):
+            bkgrnd_colours.append("#666666")
+        if avg_B >= (0.65 * max(list_of_Bs)) and avg_B <= (0.75 * max(list_of_Bs)):
+            bkgrnd_colours.append("#4D4D4D")
+        if avg_B >= (0.75 * max(list_of_Bs)) and avg_B <= (0.85 * max(list_of_Bs)):
+            bkgrnd_colours.append("#333333")
+        if avg_B >= (0.85 * max(list_of_Bs)) and avg_B <= (0.95 * max(list_of_Bs)):
+            bkgrnd_colours.append("#1A1A1A")
+        if avg_B >= (0.95 * max(list_of_Bs)):
+            bkgrnd_colours.append("#000000")
+    return(bkgrnd_colours)
 
 def write_boilerplate_start(filename = "2DREP_Output.html"):
     with open(filename, "a") as output: #use "a" to append, prevents overwriting.
@@ -82,8 +88,7 @@ def write_boilerplate_start(filename = "2DREP_Output.html"):
                 <body>
                 <img src="logo_and_legend.png" alt="2DREP Logo" style="width:100%;height:40%;">
                 """
-                ) #Working title is 2DREP
-                #logo styling is not working inline but other bits are...
+                ) 
 
 def write_boilerplate_end(filename = "2DREP_Output.html"):
     with open(filename, "a") as output: #"a" to append to end of sequence output.
@@ -104,8 +109,10 @@ def write_data(filename = "2DREP_Output.html", molecule = None):
             output.write(str(polymer.id().trim()))
             output.write(": </div>")
             bfacs_list = [] #we'll pull a B factor for display later
-            for monomer in polymer: #need to act before our residues are a string
-                bfacs_list.append(calc_avg_B(monomer)) #adds the b factor for each residue in the ATOM list - will match our sequence!
+            for monomer in polymer:
+                if monomer.type().trim() in residues: #need to act before our residues are a string
+                    bfacs_list.append(calc_avg_B(monomer)) #adds the b factor for each residue in the ATOM list - will match our sequence!
+            background_shades = background_B_colour(bfacs_list)
             sequence = fasta(polymer) #we'll write out this string styled
             for residue in sequence:
                 residue_count += 1
@@ -123,7 +130,7 @@ def write_data(filename = "2DREP_Output.html", molecule = None):
                         output.write("&nbsp;")
                     output.write("&nbsp")
                 colour = get_colour(residue)
-                background = background_B_colour(bfacs_list[residue_count - 1]) #residue count starts at 1, indices start at 0.
+                background = background_shades[residue_count - 1] #residue count starts at 1, indices start at 0.
                 output.write("<span style=\"color:" + colour + "; background-color:" + background + "\">" + residue + "</span>") #colours with the appropriate hexcode for our residue
                 counter += 1
                 if len(str(residue_count)) != 1 and len(str(residue_count)) != 2:
